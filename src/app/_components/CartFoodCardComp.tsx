c6e8b07dd6f3416f8f4d338675ddfda4a77323b5
oyunmyagmar <25LP4018@nest.edu.mn>
2025-10-26 04:43:04 +0900

    price

--- src/app/_components/CartFoodCardComp.tsx
+++ src/app/_components/CartFoodCardComp.tsx
@@ -1,104 +1,104 @@
-// "use client";
-// import React, { useState } from "react";
-// import Image from "next/image";
-// import { IoCloseOutline } from "react-icons/io5";
-// import { FiMinus, FiPlus } from "react-icons/fi";
-// import { CartFood } from "@/lib/type";
-// import { Separator, Button } from "@/components/ui";
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import { IoCloseOutline } from "react-icons/io5";
+import { FiMinus, FiPlus } from "react-icons/fi";
+import { CartFood } from "@/lib/type";
+import { Separator, Button } from "@/components/ui";
 
-// export const CartFoodCardComp = ({
-//   cartFoods,
-//   cartFood,
-//   reloadFoods,
-// }: {
-//   cartFoods: CartFood[];
-//   cartFood: CartFood;
-//   reloadFoods: () => void;
-// }) => {
-//   const [quantity, setQuantity] = useState<number>(0);
+export const CartFoodCardComp = ({
+  cartFoods,
+  cartFood,
+  reloadFoods,
+}: {
+  cartFoods: CartFood[];
+  cartFood: CartFood;
+  reloadFoods: () => void;
+}) => {
+  const [quantity, setQuantity] = useState<number>(cartFood.quantity);
 
-//   const decrementCartFoodQuant = () => {
-//     quantity > 1 && setQuantity((quantity) => quantity - 1);
-//   };
-//   const incrementCartFoodQuant = () => {
-//     setQuantity((quantity) => quantity + 1);
-//   };
+  const decrementCartFoodQuant = () => {
+    quantity > 1 && setQuantity((quantity) => quantity - 1);
+  };
+  const incrementCartFoodQuant = () => {
+    setQuantity((quantity) => quantity + 1);
+  };
 
-//   const deleteFoodFromCart = (id: string) => {
-//     // const foodsFromLocal: CartFood[] = JSON.parse(
-//     //   localStorage.getItem("cartFoods") ?? "[]"
-//     // );
+  const deleteFoodFromCart = (id: string) => {
+    // const foodsFromLocal: CartFood[] = JSON.parse(
+    //   localStorage.getItem("cartFoods") ?? "[]"
+    // );
 
-//     const remainedFoods = cartFoods.filter(
-//       (cartFood) => cartFood.food._id !== id
-//     );
-//     localStorage.setItem("cartFoods", JSON.stringify(remainedFoods));
+    const remainedFoods = cartFoods.filter(
+      (cartFood) => cartFood.food._id !== id
+    );
+    localStorage.setItem("cartFoods", JSON.stringify(remainedFoods));
 
-//     reloadFoods();
-//   };
+    reloadFoods();
+  };
 
-//   return (
-//     <div className="w-full flex flex-col gap-5">
-//       <div className="flex gap-2.5">
-//         <div className="w-31 h-30 rounded-xl relative overflow-hidden">
-//           <Image
-//             src={cartFood.food.image}
-//             alt=""
-//             width={124}
-//             height={120}
-//             className="object-cover w-full h-full"
-//             unoptimized
-//           />
-//         </div>
-//         <div className="flex-1">
-//           <div className="flex flex-col gap-6">
-//             <div className="flex gap-2.5">
-//               <div className="flex-1">
-//                 <div className="text-base leading-7 font-bold text-red-500">
-//                   {cartFood.food.foodName}
-//                 </div>
-//                 <div className="text-xs leading-4 text-foreground">
-//                   {cartFood.food.ingredients}
-//                 </div>
-//               </div>
+  return (
+    <div className="w-full flex flex-col gap-5">
+      <div className="flex gap-2.5">
+        <div className="w-31 h-30 rounded-xl relative overflow-hidden">
+          <Image
+            src={cartFood.food.image}
+            alt=""
+            width={124}
+            height={120}
+            className="object-cover w-full h-full"
+            unoptimized
+          />
+        </div>
+        <div className="flex-1">
+          <div className="flex flex-col gap-6">
+            <div className="flex gap-2.5">
+              <div className="flex-1">
+                <div className="text-base leading-7 font-bold text-red-500">
+                  {cartFood.food.foodName}
+                </div>
+                <div className="text-xs leading-4 text-foreground">
+                  {cartFood.food.ingredients}
+                </div>
+              </div>
 
-//               <div
-//                 onClick={() => deleteFoodFromCart(cartFood.food._id)}
-//                 className="w-9 h-9 rounded-full border border-red-500 bg-white flex justify-center items-center"
-//               >
-//                 <IoCloseOutline size={16} className="text-red-500" />
-//               </div>
-//             </div>
+              <div
+                onClick={() => deleteFoodFromCart(cartFood.food._id)}
+                className="w-9 h-9 rounded-full border border-red-500 bg-white flex justify-center items-center"
+              >
+                <IoCloseOutline size={16} className="text-red-500" />
+              </div>
+            </div>
 
-//             <div className="flex justify-between text-foreground items-center">
-//               <div className="flex items-center gap-3">
-//                 <Button
-//                   onClick={decrementCartFoodQuant}
-//                   variant={"ghost"}
-//                   className="size-9"
-//                 >
-//                   <FiMinus size={16} />
-//                 </Button>
-//                 <div className="text-lg leading-7 font-semibold">
-//                   {quantity}
-//                 </div>
-//                 <Button
-//                   onClick={incrementCartFoodQuant}
-//                   variant={"ghost"}
-//                   className="size-9"
-//                 >
-//                   <FiPlus size={16} />
-//                 </Button>
-//               </div>
+            <div className="flex justify-between text-foreground items-center">
+              <div className="flex items-center gap-3">
+                <Button
+                  onClick={decrementCartFoodQuant}
+                  variant={"ghost"}
+                  className="size-9"
+                >
+                  <FiMinus size={16} />
+                </Button>
+                <div className="text-lg leading-7 font-semibold">
+                  {quantity}
+                </div>
+                <Button
+                  onClick={incrementCartFoodQuant}
+                  variant={"ghost"}
+                  className="size-9"
+                >
+                  <FiPlus size={16} />
+                </Button>
+              </div>
 
-//               <div className="text-base leading-7 font-bold">
-//                 ${cartFood.food.price * quantity}
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//       <Separator className="border border-dashed border-[rgba(9,9,11,0.5)] bg-transparent" />
-//     </div>
-//   );
-// };
+              <div className="text-base leading-7 font-bold">
+                ${cartFood.food.price * quantity}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Separator className="border border-dashed border-[rgba(9,9,11,0.5)] bg-transparent" />
+    </div>
+  );
+};
--- src/app/_components/ShoppingCart.tsx
+++ src/app/_components/ShoppingCart.tsx
@@ -17,24 +17,23 @@ import {
 } from "@/components/ui";
 import { LuShoppingCart } from "react-icons/lu";
 import { CartFood } from "@/lib/type";
-import { LogoImgShoppingCart, OrderAlertDialog } from "@/app/_components";
-import Image from "next/image";
-import { FiMinus, FiPlus } from "react-icons/fi";
-import { IoCloseOutline } from "react-icons/io5";
+import {
+  CartFoodCardComp,
+  LogoImgShoppingCart,
+  OrderAlertDialog,
+} from "@/app/_components";
 
 export const ShoppingCart = ({ email }: { email: string }) => {
   const [cartOpen, setCartOpen] = useState<boolean>(false);
   const [cartFoods, setCartFoods] = useState<CartFood[]>([]);
   const [userId, setUserId] = useState<string>("");
   const [address, setAddress] = useState<string>("");
-  const [quantity, setQuantity] = useState<number>(0);
   const shippingPrice = 0.99;
   let cartFoodsPriceBeforeShipping: number = 0;
   const [showAlertDialog, setShowAlertDialog] = useState(false);
 
   cartFoods.forEach((cartFood) => {
-    setQuantity(cartFood.quantity);
-    const foodUnitsPrice = cartFood.food.price * quantity;
+    const foodUnitsPrice = cartFood.food.price * cartFood.quantity;
     cartFoodsPriceBeforeShipping += foodUnitsPrice;
   });
 
@@ -45,26 +44,6 @@ export const ShoppingCart = ({ email }: { email: string }) => {
       localStorage.getItem("cartFoods") ?? "[]"
     );
     setCartFoods(cartFoodsFromLocal);
-  };
-
-  const decrementCartFoodQuant = () => {
-    quantity > 1 && setQuantity((quantity) => quantity - 1);
-  };
-  const incrementCartFoodQuant = () => {
-    setQuantity((quantity) => quantity + 1);
-  };
-
-  const deleteFoodFromCart = (id: string) => {
-    // const foodsFromLocal: CartFood[] = JSON.parse(
-    //   localStorage.getItem("cartFoods") ?? "[]"
-    // );
-
-    const remainedFoods = cartFoods.filter(
-      (cartFood) => cartFood.food._id !== id
-    );
-    localStorage.setItem("cartFoods", JSON.stringify(remainedFoods));
-
-    reloadFoods();
   };
 
   useEffect(() => {
@@ -142,7 +121,7 @@ export const ShoppingCart = ({ email }: { email: string }) => {
           </Button>
         </DrawerTrigger>
 
-        <DrawerContent className="data-[vaul-drawer-direction=right]:sm:max-w-[535px] p-8 gap-6 bg-neutral-700 text-primary-foreground border-none rounded-tl-[20px] rounded-bl-[20px]">
+        <DrawerContent className="data-[vaul-drawer-direction=right]:sm:max-w-[535px] p-8 gap-6 bg-neutral-700 text-primary-foreground border-none rounded-tl-[20px] rounded-bl-[20px] overflow-y-auto">
           <DrawerHeader className="flex-row gap-3 items-center py-1">
             <div>
               <LuShoppingCart size={24} />
@@ -185,79 +164,12 @@ export const ShoppingCart = ({ email }: { email: string }) => {
 
                     {cartFoods.length > 0 ? (
                       cartFoods.map((cartFood) => (
-                        // <CartFoodCardComp
-                        //   cartFoods={cartFoods}
-                        //   cartFood={cartFood}
-                        //   reloadFoods={reloadFoods}
-                        //   key={cartFood.food._id}
-                        // />
-                        <div className="w-full flex flex-col gap-5">
-                          <div className="flex gap-2.5">
-                            <div className="w-31 h-30 rounded-xl relative overflow-hidden">
-                              <Image
-                                src={cartFood.food.image}
-                                alt=""
-                                width={124}
-                                height={120}
-                                className="object-cover w-full h-full"
-                                unoptimized
-                              />
-                            </div>
-                            <div className="flex-1">
-                              <div className="flex flex-col gap-6">
-                                <div className="flex gap-2.5">
-                                  <div className="flex-1">
-                                    <div className="text-base leading-7 font-bold text-red-500">
-                                      {cartFood.food.foodName}
-                                    </div>
-                                    <div className="text-xs leading-4 text-foreground">
-                                      {cartFood.food.ingredients}
-                                    </div>
-                                  </div>
-
-                                  <div
-                                    onClick={() =>
-                                      deleteFoodFromCart(cartFood.food._id)
-                                    }
-                                    className="w-9 h-9 rounded-full border border-red-500 bg-white flex justify-center items-center"
-                                  >
-                                    <IoCloseOutline
-                                      size={16}
-                                      className="text-red-500"
-                                    />
-                                  </div>
-                                </div>
-
-                                <div className="flex justify-between text-foreground items-center">
-                                  <div className="flex items-center gap-3">
-                                    <Button
-                                      onClick={decrementCartFoodQuant}
-                                      variant={"ghost"}
-                                      className="size-9"
-                                    >
-                                      <FiMinus size={16} />
-                                    </Button>
-                                    <div className="text-lg leading-7 font-semibold">
-                                      {quantity}
-                                    </div>
-                                    <Button
-                                      onClick={incrementCartFoodQuant}
-                                      variant={"ghost"}
-                                      className="size-9"
-                                    >
-                                      <FiPlus size={16} />
-                                    </Button>
-                                  </div>
-
-                                  <div className="text-base leading-7 font-bold">
-                                    ${cartFood.food.price * quantity}
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          <Separator className="border border-dashed border-[rgba(9,9,11,0.5)] bg-transparent" />
-                        </div>
+                        <CartFoodCardComp
+                          cartFoods={cartFoods}
+                          cartFood={cartFood}
+                          reloadFoods={reloadFoods}
+                          key={cartFood.food._id}
+                        />
                       ))
                     ) : (
                       <div className="py-8 px-12 flex flex-col gap-1 bg-secondary rounded-xl items-center">
